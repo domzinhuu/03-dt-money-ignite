963314c57b8a981d645397ad7e2aec73f9cdeda0
Domzinhuu <maique.rosa@gmail.com>
2023-06-19 18:09:25 -0300

    feat: alterando chamada de api na pagina de transaçoes para o context API

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,31 +1,16 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
-import { PriceHighlight, TransactionContainer, TransactionsTable } from "./styles";
-
-interface Transaction {
-  id: number;
-  description: string;
-  type: "income" | "outcome";
-  price: number;
-  category: string;
-  createdAt: string;
-}
+import {
+  PriceHighlight,
+  TransactionContainer,
+  TransactionsTable,
+} from "./styles";
+import { TransactionContext } from "../../contexts/TransactionContext";
 
 export function Transactions() {
-  const [transaction, setTransactions] = useState<Transaction[]>([]);
-
-  async function loadTransactions() {
-    const response = await fetch("http://localhost:3333/transactions");
-    const data = await response.json();
-    console.log(data);
-    setTransactions(data);
-  }
-
-  useEffect(() => {
-    loadTransactions();
-  }, []);
+  const { transactions } = useContext(TransactionContext);
 
   return (
     <div>
@@ -36,11 +21,13 @@ export function Transactions() {
         <SearchForm />
         <TransactionsTable>
           <tbody>
-            {transaction.map((item) => (
+            {transactions.map((item) => (
               <tr key={item.id}>
                 <td width="50%">{item.description}</td>
                 <td>
-                  <PriceHighlight variant={item.type}>{item.price}</PriceHighlight>
+                  <PriceHighlight variant={item.type}>
+                    {item.price}
+                  </PriceHighlight>
                 </td>
                 <td>{item.category}</td>
                 <td>{item.createdAt}</td>
